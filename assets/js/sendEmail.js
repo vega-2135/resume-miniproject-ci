235a737f449ee648df8441a27adb9dd7952dc3a7
Carolina Romero Carrasco <mcarolinarc28@gmail.com>
2024-02-28 10:56:59 +0100

    Implement emailjs integration on contact page

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,16 @@
+function sendMail(contactForm) {
+    emailjs.send("service_sz2psb7","template_k52q06o", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "message": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    
+}
